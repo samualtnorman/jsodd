d0ce43ff6e04a9668ad813d4a51d9c2139a1a9a7
Samual Norman <me@samual.uk>
2025-09-04 11:36:24 +0100

    `HERE`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,18 @@
+import babelPluginSyntaxTypescript from "@babel/plugin-syntax-typescript"
+import { babel } from "@rollup/plugin-babel"
+import { babelPluginHere } from "babel-plugin-here"
 import type { ViteUserConfig } from "vitest/config"
 
-export default { test: { includeSource: [ "src/**/*.ts" ] } } satisfies ViteUserConfig
+export default {
+	test: { includeSource: [ "src/**/*.ts" ] },
+	plugins: [
+		{
+			...babel({
+				babelHelpers: "bundled",
+				extensions: [ ".ts" ],
+				plugins: [ babelPluginSyntaxTypescript, babelPluginHere() ]
+			}) as any,
+			enforce: "pre"
+		}
+	]
+} satisfies ViteUserConfig
