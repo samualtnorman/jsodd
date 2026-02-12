d14978180f5ddf2e2a3a56fc0b5792fa70cfbb59
Samual Norman <me@samual.uk>
2026-02-12 13:46:48 +0000

    update config to allow testing bundle and minifying

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,4 +1,12 @@
 #!node_modules/.bin/rolldown -c
 import { rolldownConfig } from "@samual/rolldown-config"
 
-export default rolldownConfig()
+export default rolldownConfig(
+	process.env.TEST_BUNDLE_AND_MINIFY
+		? {
+			rolldownOptions: { external: [] },
+			terserOptions: { mangle: { keep_classnames: true, keep_fnames: true } },
+			experimental: { disablePrettier: true }
+		}
+		: undefined
+)
